8b80d0efec686ea28c165b757e1ee95bea250e02
Yulia <y.chikaleva99@mail.ru>
2023-10-01 11:35:23 +0300

    Добавили цикл в 1 задание

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -3,11 +3,15 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 double GetPower(double a,double b) {
-    double result = Math.Pow(a, b);
- return result;
-}
+    if (b != 0){
+    double result = 1;
+    for (int i=1; i<=b; i++){
+        result = result*a;
+    } return result;
+    } else {return 1;}
+ }
 Console.WriteLine("Введите число");
 int x = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число");
+Console.WriteLine("Введите число больше 0");
 int y = Convert.ToInt32(Console.ReadLine());
 Console.Write(GetPower(x,y));
